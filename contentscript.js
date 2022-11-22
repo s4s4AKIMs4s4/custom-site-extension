1f6c208f13679f3dfd64cd1cad94bb6fcb1106e9
Ivan <akimovivan355@gmail.com>
2022-11-22 20:59:05 +0700

    add new style for all domen

--- contentscript.js
+++ contentscript.js
@@ -1,34 +1,16 @@
 function rendoringTemplate(selectors, color, callbackForRedor) {
   selectors.forEach((val) => {
-    callbackForRedor(val, color);
+    const allEments = document.querySelectorAll(val);
+    callbackForRedor(val, color, allEments);
   });
 }
 
 class commonTemplate {
-  template = (selector, color) => {
-    console.log("commonTemplate");
-    const allEments = document.querySelectorAll(selector);
+  template = (selector, color, allEments) => {
     allEments.forEach((val, idx) => {
-      if (
-        selector === "yt-formatted-string" ||
-        selector === ".ytd-video-meta-block"
-      ) {
-        val.style.color = `rgba(${color.r}, ${color.g}, ${color.b},${0.7})`;
-        return;
-      }
       if (val.children.length === 0)
         val.style.color = `rgb(${color.r}, ${color.g}, ${color.b})`;
-      else if (
-        selector === "h3+div" ||
-        selector === "a *" ||
-        selector === "a" ||
-        selector === 'div[role="heading"]' ||
-        selector === "h3+span"
-      ) {
-        val.style.color = `rgb(${color.r}, ${color.g}, ${color.b})`;
-      } else if (selector === "yt-formatted-string") {
-        console.log("selectorsssssss");
-        console.log(selector);
+      else if (selector === "yt-formatted-string") {
         return;
       } else {
         if (val.children[0].tagName === "EM")
@@ -65,57 +47,39 @@ class Google extends commonTemplate {
       '*[data-content-feature="1"] div',
       "h3+div",
       "h3+span",
+      'div[role="heading"]'
     ];
-    rendoringTemplate(selectors, color, this.template);
+
+    const paintTextHOC = (callback) => {
+      return (selector, color, allEments) => {
+        if (
+          selector === "h3+div" ||
+          selector === "a *" ||
+          selector === "a" ||
+          selector === 'div[role="heading"]' ||
+          selector === "h3+span"
+        ) {
+          for (let i = 0; i < allEments.length; i++) {
+            allEments[
+              i
+            ].style.color = `rgb(${color.r}, ${color.g}, ${color.b})`;
+          }
+          return;
+        }
+        callback(selector, color, allEments);
+      };
+    };
+
+    rendoringTemplate(selectors, color, paintTextHOC(this.template));
   }
 
   performLinks(color) {
     const selectors = ["a", "h3", "cite", "a *", 'div[role="heading"]'];
     rendoringTemplate(selectors, color, this.template);
-    // this.template('a', color)
   }
 
   performInput(color) {
     this.templateBorderColor(".RNNXgb", color);
-  }
-
-  performBackground(color) {
-    const BackgroundSelecors = [
-      "body",
-      ".I6TXqe ",
-      ".nm6nmc",
-      ".Lj180d",
-      ".y8Jpof.kpQuGf",
-      ".nm6nmc.kpQuGf",
-      ".sfbg",
-      ".yg51vc",
-      ".f6F9Be",
-      "WE0UJf",
-      "#extabar",
-      ".MXl0lf.mtqGb",
-      ".aajZCb",
-      ".Lj9fsd",
-      ".tAcEof",
-      ".cj2HCb",
-      ".mR2gOd",
-      ".FalWJb",
-      ".c93Gbe",
-      ".mnr-c.pla-unit",
-      "html",
-      ".s8GCU",
-      ".jZWadf",
-      "g-inner-card",
-      "ytd-author-comment-badge-renderer",
-      ".ytp-paid-content-overlay",
-      ".ytp-spinner",
-      ".ifM9O",
-      ".cG5GOd",
-      ".GHDvEf",
-      ".gb_pa.gb_Zd.gb_Va.gb_vc.gb_Qd",
-      ".U1hlv.Aajd3",
-    ];
-
-    rendoringTemplate(BackgroundSelecors, color, this.templateBackgroundColor);
   }
 }
 
@@ -141,132 +105,6 @@ class Youtube extends commonTemplate {
     const selectors = ["#container", "#search-icon-legacy"];
     rendoringTemplate(selectors, color, this.templateBorderColor);
   }
-
-  performBackground(color) {
-    this.templateBackgroundColor(
-      "canvas,#endpoint,.style-scope.ytd-app, caption, center, cite, code,dd, del, dfn, dl, dt, em, embed, fieldset, font, form, h1, h2, h3, h4, h5, h6, hr, i, iframe, img, ins, kbd, label, legend, li, menu, object, ol, p, pre, q, s, samp, small, span, strike, strong, sub, sup, table, tbody, td, tfoot, th, thead, tr, tt, u, ul, var, #items",
-      color
-    );
-
-    document.querySelector(
-      "#container.ytd-masthead"
-    ).style.backgroundColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
-    if (document.querySelector("#chips.ytd-feed-filter-chip-bar-renderer"))
-      document.querySelector(
-        "#chips.ytd-feed-filter-chip-bar-renderer"
-      ).style.backgroundColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
-    if (document.querySelector("ytd-mini-guide-entry-renderer"))
-      document.querySelector(
-        "ytd-mini-guide-entry-renderer"
-      ).style.backgroundColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
-    if (
-      document.querySelector(".ytp-exp-bottom-control-flexbox .ytp-volume-area")
-    )
-      document.querySelector(
-        ".ytp-exp-bottom-control-flexbox .ytp-volume-area"
-      ).style.backgroundColor = "transparent";
-    if (document.querySelector(".ytp-chapter-title-prefix"))
-      document.querySelector(
-        ".ytp-chapter-title-prefix"
-      ).style.backgroundColor = "transparent";
-    if (
-      document.querySelector(
-        "#right-arrow-button.ytd-feed-filter-chip-bar-renderer"
-      )
-    )
-      document.querySelector(
-        "#right-arrow-button.ytd-feed-filter-chip-bar-renderer"
-      ).style.background = `rgb(${color.r}, ${color.g}, ${color.b})`;
-    if (
-      document.querySelector(
-        "#right-arrow.ytd-feed-filter-chip-bar-renderer:before"
-      )
-    )
-      document.querySelector(
-        "#right-arrow.ytd-feed-filter-chip-bar-renderer:before"
-      ).style.background = `rgb(${color.r}, ${color.g}, ${color.b})`;
-    if (
-      document.querySelector(
-        "ytd-browse[mini-guide-visible] ytd-playlist-sidebar-renderer.ytd-browse"
-      )
-    )
-      document.querySelector(
-        "ytd-browse[mini-guide-visible] ytd-playlist-sidebar-renderer.ytd-browse"
-      ).style.background = `rgb(${color.r}, ${color.g}, ${color.b})`;
-    if (document.querySelector("ytd-item-section-renderer"))
-      document.querySelector(
-        "ytd-item-section-renderer"
-      ).style.background = `rgb(${color.r}, ${color.g}, ${color.b})`;
-    if (
-      document.querySelector(
-        "ytd-playlist-sidebar-renderer.ytd-browse, ytd-settings-sidebar-renderer.ytd-browse"
-      )
-    )
-      document.querySelector(
-        "ytd-playlist-sidebar-renderer.ytd-browse, ytd-settings-sidebar-renderer.ytd-browse"
-      ).style.background = `rgb(${color.r}, ${color.g}, ${color.b})`;
-    if (
-      document.querySelector(
-        "ytd-browse[page-subtype=playlist][has-sidebar_], ytd-browse[page-subtype=show][has-sidebar_]"
-      )
-    )
-      document.querySelector(
-        "ytd-browse[page-subtype=playlist][has-sidebar_], ytd-browse[page-subtype=show][has-sidebar_]"
-      ).style.background = `rgb(${color.r}, ${color.g}, ${color.b})`;
-    if (document.querySelector("ytd-page-manager > *.ytd-page-manager"))
-      document.querySelector(
-        "ytd-page-manager > *.ytd-page-manager"
-      ).style.background = `rgb(${color.r}, ${color.g}, ${color.b})`;
-    if (
-      document.querySelector(
-        "ytd-c4-tabbed-header-renderer.grid-4-columns #channel-header.ytd-c4-tabbed-header-renderer"
-      )
-    )
-      document.querySelector(
-        "ytd-c4-tabbed-header-renderer.grid-4-columns #channel-header.ytd-c4-tabbed-header-renderer"
-      ).style.background = `rgb(${color.r}, ${color.g}, ${color.b})`;
-
-    if (document.querySelector(".sbsb_a"))
-      document.querySelector(
-        ".sbsb_a"
-      ).style.background = `rgb(${color.r}, ${color.g}, ${color.b})`;
-
-    if (document.querySelector(".sbfl_b "))
-      document.querySelector(
-        ".sbfl_b "
-      ).style.backgroundColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
-    if (document.querySelector(".sbfl_b "))
-      document.querySelector(
-        ".sbfl_b "
-      ).style.background = `rgb(${color.r}, ${color.g}, ${color.b})`;
-    // if(document.querySelector('#tabs-inner-container.ytd-c4-tabbed-header-renderer '))
-    // document.querySelector('#tabs-inner-container.ytd-c4-tabbed-header-renderer').style.background = `rgb(${color.r}, ${color.g}, ${color.b})`
-
-    if (
-      document.querySelector(
-        "ytd-two-column-browse-results-renderer[page-subtype=history] #secondary.ytd-two-column-browse-results-renderer "
-      )
-    )
-      document.querySelector(
-        "ytd-two-column-browse-results-renderer[page-subtype=history] #secondary.ytd-two-column-browse-results-renderer"
-      ).style.background = `rgb(${color.r}, ${color.g}, ${color.b})`;
-    if (
-      document.querySelector(
-        "#tabs-inner-container.ytd-c4-tabbed-header-renderer "
-      )
-    )
-      if (
-        document.querySelector(
-          "ytd-browse[guide-persistent-and-visible] ytd-playlist-sidebar-renderer.ytd-browse"
-        )
-      )
-        document.querySelector(
-          "ytd-browse[guide-persistent-and-visible] ytd-playlist-sidebar-renderer.ytd-browse"
-        ).style.background = `rgb(${color.r}, ${color.g}, ${color.b})`;
-    document.querySelector(
-      "ytd-page-manager > *.ytd-page-manager"
-    ).style.background = `rgb(${color.r}, ${color.g}, ${color.b})`;
-  }
 }
 
 class ScrollUpadtaAlgoritm extends commonTemplate {
@@ -287,9 +125,28 @@ class ScrollUpadtaAlgoritm extends commonTemplate {
       "sbqs_c",
       "b",
       ".ytd-video-meta-block",
-      "h1 yt-formatted-string"
+      "h1 yt-formatted-string",
+      "#video-title",
+      ".ytd-video-renderer"
     ];
-    rendoringTemplate(selectors, this._text, this.template);
+
+    const paintTextHOC = (callback) => {
+      return (selector, color, allEments) => {
+          if (
+            selector === "yt-formatted-string" ||
+            selector === ".ytd-video-meta-block"
+          ) {
+            for (let i = 0; i < allEments.length; i++) {
+              allEments[i].style.color = `rgba(${color.r}, ${color.g}, ${
+                color.b
+              },${0.7})`;
+            }
+            return;
+          }
+        callback(selector, color, allEments);
+      };
+    };
+    rendoringTemplate(selectors, this._text, paintTextHOC(this.template));
   }
 
   paintLink() {
@@ -353,9 +210,6 @@ class Director {
       case "input":
         this._domen.performInput(color);
         break;
-      case "backgrounds":
-        this._domen.performBackground(color);
-        break;
     }
   }
 
@@ -379,10 +233,6 @@ const rewriteKeys = (currentDomain) => {
 };
 
 const selectObjectForDomen = (currentDomain, state) => {
-  //   const domain = rewriteKeys(currentDomain);
-  //   console.log('domain')
-  //   console.log(domain)
-
   try {
     if (!checkKeysForLength(state, currentDomain)) {
       switch (currentDomain) {
@@ -408,7 +258,6 @@ chrome.runtime.onMessage.addListener(async function (
   domain = selectObjectForDomen(currentDomain, request.state);
   if (domain) {
     director = new Director(domain);
-    console.log("domain found");
     director.performAllArea(request.state[currentDomain]);
   }
 });
